eba1a6a9e57239f068198ecc6681515a65bb6ac7
kevinene91 <kevinene91@gmail.com>
2018-08-31 21:24:02 +0300

    [feature] admin crud #160091542

--- UI/assets/js/app.js
+++ UI/assets/js/app.js
@@ -14,4 +14,26 @@ for (var i = 0; i < collapsible.length; i++){
     });
 
     }
+
+
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
   
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    
